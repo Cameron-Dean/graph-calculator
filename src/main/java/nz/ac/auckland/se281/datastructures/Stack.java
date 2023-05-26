91c5d08ae71f119552bb41df3964be4e34523d1c
Cameron Dean <camerondeannz@gmail.com>
2023-05-26 22:53:31 +1200

    Added javadoc comments to methods in the stack class

--- src/main/java/nz/ac/auckland/se281/datastructures/Stack.java
+++ src/main/java/nz/ac/auckland/se281/datastructures/Stack.java
@@ -10,16 +10,32 @@ public class Stack<T extends Comparable<T>> extends LinkedList<T> {
     super();
   }
 
+  /**
+   * Adds an element with the specified value to the top of the stack.
+   *
+   * @param value The value to add to the top of the stack.
+   */
   public void push(T value) {
     super.prepend(value);
   }
 
+  /**
+   * Removes the element at the top of the stack and returns the value that was stored in that
+   * element.
+   *
+   * @return The value of the element at the top of the stack previously.
+   */
   public T pop() {
     Node<T> result = super.head;
     super.remove(0);
     return result.getValue();
   }
 
+  /**
+   * Returns the value of the element at the top of the stack currently.
+   *
+   * @return The value of the element at the top of the stack currently.
+   */
   public T peek() {
     return super.head.getValue();
   }
